8368064549fec3160676ec02c312c935e134a778
dev-ezzy <bettezra07@gmail.com>
2024-01-21 15:17:35 +0300

    commit

--- functions2.py
+++ functions2.py
@@ -1,3 +1,5 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
 def sns_ycount(column , data):
     sns.countplot(y = column, data = data, hue= column)
     plt.title(f"{column} count in our data set")
