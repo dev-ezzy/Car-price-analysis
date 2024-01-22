cb94b75ddd54cc871e5a22c80a54822ba74c6bb3
dev-ezzy <bettezra07@gmail.com>
2024-01-22 18:27:25 +0300

    commit

--- functions2.py
+++ functions2.py
@@ -1,5 +1,9 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
+
+#loading our data
+cars = pd.read_csv("data/CarPrice_Assignment.csv")
 def sns_ycount(column , data):
     sns.countplot(y = column, data = data, hue= column)
     plt.title(f"{column} count in our data set")
@@ -11,7 +15,7 @@ def scatter_plots(y, X):
     cols = 4
     rows = (plots + cols - 1) // cols
 
-    fig, axes = plt.subplots(rows, cols, figsize=(15, 6))
+    fig, axes = plt.subplots(rows, cols, figsize=(15, 5 * rows))
     fig.suptitle(f"Scatter plot of Independent variables vs {y.name}")
 
     for i, ax in enumerate(axes.flat):
@@ -21,4 +25,11 @@ def scatter_plots(y, X):
             ax.set_xlabel(x_col_name)
             ax.set_ylabel(y.name)
             ax.set_title(f"{x_col_name} vs {y.name}")
+            fig.subplots_adjust(wspace=0.4, hspace=0.4)
+            plt.tight_layout()
+            plt.show()
             
+#scatter plots of two variables
+def scatter(X, y):
+    sns.scatterplot(x = X, y= y, data= cars)
+    plt.title(f"Relationship of {X} and {y}")
