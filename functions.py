fdc417bc9fecda85272e131682ee4e8844c1be33
dev-ezzy <bettezra07@gmail.com>
2024-01-23 17:28:26 +0300

    commit

--- functions.py
+++ functions.py
@@ -60,7 +60,7 @@ def plot_histograms(data):
 def box_pair_plot(column):
     fig, axes = plt.subplots(nrows= 1, ncols= 2, figsize = (15, 5))
     sns.boxplot(y= "price", x = column, data= cars, hue= None, ax= axes[0])
-    axes[0].title(f"Box_plots of {column} against price")
+    plt.title(f"Box_plots of {column} against price", ax = axes[0])
     sns.pointplot(y= "price", x = column, data= cars, ax= axes[1])
     axes[1].title(f"Point_plots of {column} against price")
     plt.show()
